20e81cad6ecc9d8f5b9bf24e3558befc2db4752d
Jesse Box <me@jessebox.net>
2023-09-12 17:07:55 +0200

    Modified: styles.css

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,21 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Send Feedback</h1>
-<form>
-  <label for="full-name">Full Name</label>
-  <input type="text" id="full-name" name="full-name" placeholder="Full Name">
-  <label for="email">Email</label>
-  <input type="email" id="email" name="email" placeholder="Email" required/>
-  <label for="description">Description</label>
-  <textarea id="email" name="email" placeholder="Whats the issue? What did you expect?" rows=8 required></textarea>
-  <input type="submit" value="Send Feedback">
-</form>
+<section class="uf-base uf-container">
+  <div class=uf-container-header>
+    <h3 class="uf-heading">Share Feedback</h3>
+  </div>
+  <form id="form" class="uf-container-main">
+    <label class="uf-label" for="fullName">Full Name</label>
+    <input type="text" id="fullName" class="uf-input" name="fullName" placeholder="Full Name">
+    <label class="uf-label" for="email">Email</label>
+    <input type="email" id="email" class="uf-input" name="email" placeholder="Email" required/>
+    <label class="uf-label" for="description">Description</label>
+    <textarea id="description" class="uf-textarea" name="description" placeholder="Whats the issue? What did you expect?" rows=8 required></textarea>
+  </form>
+  <section class="uf-container-footer">
+    <input type="submit" form="form" class="uf-button uf-button-accent" value="Send Feedback"> 
+    <button type="button" class="uf-button">Cancel</button>
+  </section>
+</section>
 `;
